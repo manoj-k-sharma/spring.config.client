12e4b9b1a1b2863943efd13bfaea1bf122c87976
manoj-k-sharma <manoj.sharma.c@ephesoft.com>
2020-04-15 22:11:39 +0530

    firt ms

---
+++ src/main/java/spring/config/client/com/ConfigClientRest.java
@@ -0,0 +1,18 @@
+package spring.config.client.com;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigClientRest {
+	
+	@Value("${name}")
+	String name;
+	
+	@RequestMapping("/getName")
+	public void get() {
+		System.out.println("** name is ** "+name);
+	}
+
+}
---
+++ src/main/java/spring/config/client/com/SpringConfigClient.java
@@ -0,0 +1,14 @@
+package spring.config.client.com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringConfigClient {
+	
+	public static void main(String arg[]) {
+		
+		SpringApplication.run(SpringConfigClient.class, arg);
+	}
+
+}
